dd05d8c480f4a49569ae3ed39c2da3a88138b429
PoWeR4Chan <63404242+PoWeR4Chan@users.noreply.github.com>
2021-07-27 23:56:09 +0900

    Update rmove-swst-1.sh

--- rmove-swst-1.sh
+++ rmove-swst-1.sh
@@ -6,4 +6,3 @@ find ./ -mmin +5 -type f -name '*-ST*.mp4' -not -path "./TV/Downloads/*" | renam
 
 find ./ -mmin +5 -type f -name '*-SW*.mp4' -not -path "./TV/Downloads/*" | rename -f 's/.1080p-SW//'
 find ./ -mmin +5 -type f -name '*-SW*.mp4' -not -path "./TV/Downloads/*" | rename -f 's/-SW//'
-
