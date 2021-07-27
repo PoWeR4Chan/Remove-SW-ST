e4ca1f1742c9eb838fa613ba45c102f2596d64f9
PoWeR4Chan <63404242+PoWeR4Chan@users.noreply.github.com>
2021-07-27 23:59:55 +0900

    Update rmove-swst-2.sh

--- rmove-swst-2.sh
+++ rmove-swst-2.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+# rename 몀령어가 불가능한 경우.
+
 cd $1
 find ./ -mmin +5 -type f -name '*-ST*.mp4' -not -path "./Downloads/*" | xargs -I{} sh -c 'mv -v "$0" "${0/.1080p-ST/}"' {}
 find ./ -mmin +5 -type f -name '*-ST*.mp4' -not -path "./Downloads/*" | xargs -I{} sh -c 'mv -v "$0" "${0/-ST/}"' {}
 
 find ./ -mmin +5 -type f -name '*-SW*.mp4' -not -path "./Downloads/*" | xargs -I{} sh -c 'mv -v "$0" "${0/.1080p-SW/}"' {}
 find ./ -mmin +5 -type f -name '*-SW*.mp4' -not -path "./Downloads/*" | xargs -I{} sh -c 'mv -v "$0" "${0/-SW/}"' {}
-1 
